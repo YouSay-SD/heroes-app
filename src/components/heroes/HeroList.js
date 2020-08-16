92aaeb77ae106672abbebb8256bdd46f79f410d2
YouSay-SD <yousay.sd@gmail.com>
2020-08-16 19:12:46 -0300

    Create HeroList Component

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
+
+  const heroes = getHeroesByPublisher( publisher );
+
   return (
-    <div>
-      
+    <div className="card-columns">
+
+      {
+
+        heroes.map( hero => (
+          <HeroCard 
+            key={ hero.id }
+            { ...hero }
+          />
+        ))
+
+      }
+
     </div>
   )
 }
