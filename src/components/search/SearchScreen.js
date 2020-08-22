4e23bf30c8ba98b06fe952815f7445273ea28d01
YouSay-SD <yousay.sd@gmail.com>
2020-08-22 00:19:49 -0300

    Create SearchScreen Component

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { heroes } from '../../data/heroes'
 import { HeroCard } from '../heroes/HeroCard';
+import { useForm } from '../../hooks/useForm';
 
 export const SearchScreen = () => {
 
   const heroesFiltered = heroes;
 
-  const handleSearch = () => {
-    
+  const [ formValues, handleInputChange ] = useForm({
+    searchText: ''
+  });
+
+  const { searchText } = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log( searchText );
   }
 
   return (
@@ -23,12 +31,16 @@ export const SearchScreen = () => {
           <h4>Search Form</h4>
           <hr />
 
-          <form>
+          <form onSubmit={ handleSearch }>
 
             <input 
               type="text"
               placeholder="Find your hero"
               className="form-control"
+              name="searchText"
+              value={ searchText }
+              onChange={ handleInputChange }
+              autoComplete="off"
             />
 
             <button
