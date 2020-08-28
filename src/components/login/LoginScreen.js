96b7219ebba5bf1f177eef31f923ab7297706ff2
YouSay-SD <yousay.sd@gmail.com>
2020-08-27 23:24:14 -0300

    Set UserName

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
+  const { dispatch } = useContext( AuthContext );
+
   const handleLogin = () => {
-    history.push('/');
+    // history.push('/');
+
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'YouSay',
+      },
+    })
+
+    history.replace('/');
+
   }
 
   return (
