ab496d2ed4eb295a8d9302e6d8c4113ec3104b93
YouSay-SD <yousay.sd@gmail.com>
2020-08-29 20:14:51 -0300

    Create Private Route

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from 'react'
+import React, { useContext } from 'react';
 import { AuthContext } from '../../auth/AuthContext';
 import { types } from '../../types/types';
 
